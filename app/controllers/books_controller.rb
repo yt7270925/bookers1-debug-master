ac7eec65a3a294e93ece9b536dcf839bfb059d7f
EC2 Default User <ec2-user@ip-172-31-16-77.ec2.internal>
2022-07-11 02:55:15 +0000

    編集が反映されない

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -37,8 +37,10 @@ class BooksController < ApplicationController
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      flash[:notice] = 'Book was successfully destroyed.'
+      redirect_to books_path
+    end
   end
 
   private
